655f5105e00cc716f415a12551ed15724e13a2ba
Aaron Stockdale <shadowstock76@gmail.com>
2022-09-29 12:14:51 -0400

    Added a little more CSS

--- main.js
+++ main.js
@@ -2,68 +2,66 @@ let form = document.querySelector("#form");
 let artistBand = document.querySelector("#bandBar");
 // console.log(artistBand);
 
-
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let search = artistBand.value;
-  const url = `https://proxy-itunes-api.glitch.me/search?term=${search}&limit=15`;
+  const url = `https://proxy-itunes-api.glitch.me/search?term=${search}&limit=16`;
   getData(url);
   artistBand.value = "";
 });
 
 function getData(url) {
-    fetch(url, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      })
-        // fetch gives us back a response in json format when it is ready
-        // when you get a response back from the fetch, do something with it
-        // we are naming the response `response`
-        .then(function (response) {
-          // parse the response
-          return response.json();
-        })
-        // take what just got returned (response.json()) and do something with it
-        // we assign response.json() to the variable `parsedResponse`
-        .then(function (data) {
-            displayArray(data.results);
-        //   console.log(data.results);
-        });
+  fetch(url, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    // fetch gives us back a response in json format when it is ready
+    // when you get a response back from the fetch, do something with it
+    // we are naming the response `response`
+    .then(function (response) {
+      // parse the response
+      return response.json();
+    })
+    // take what just got returned (response.json()) and do something with it
+    // we assign response.json() to the variable `parsedResponse`
+    .then(function (data) {
+      displayArray(data.results);
+      //   console.log(data.results);
+    });
 }
 
 let displayResults = document.querySelector("#displayResults");
 
 function displayArray(dataArray) {
-    displayResults.innerHTML = "";
-    for (let data of dataArray) {
-        let cardDiv = document.createElement("div");
-        let albumArt = document.createElement("img");
-        let artistName = document.createElement("h2");
-        let trackTitle = document.createElement("p");
-        let playButton = document.createElement("audio");
+  displayResults.innerHTML = "";
+  for (let data of dataArray) {
+    let cardDiv = document.createElement("div");
+    let albumArt = document.createElement("img");
+    let artistName = document.createElement("h2");
+    let trackTitle = document.createElement("p");
+    let playButton = document.createElement("audio");
 
-        albumArt.src = `${data.artworkUrl100}`;
-        artistName.innerText = `${data.artistName}`;
-        trackTitle.innerText = `${data.trackName}`;
-        playButton.src = `${data.previewUrl}`;
-        playButton.controls = true;
+    albumArt.src = `${data.artworkUrl100}`;
+    artistName.innerText = `${data.artistName}`;
+    trackTitle.innerText = `${data.trackName}`;
+    playButton.src = `${data.previewUrl}`;
+    playButton.controls = true;
 
-        cardDiv.appendChild(albumArt);
-        cardDiv.appendChild(artistName);
-        cardDiv.appendChild(trackTitle);
-        cardDiv.appendChild(playButton);
-        displayResults.appendChild(cardDiv);
+    cardDiv.appendChild(albumArt);
+    cardDiv.appendChild(artistName);
+    cardDiv.appendChild(trackTitle);
+    cardDiv.appendChild(playButton);
+    displayResults.appendChild(cardDiv);
 
-        cardDiv.classList.add("divStyle")
-    }
-    }
+    cardDiv.classList.add("divStyle");
+  }
+}
 
-    // function validateForm(){
-    //     let input = document.myForm.myBandBar.value;
-    //     if (input == null || input == ""){
-    //         alert ("Please enter an artist or band name");
-    //         alert false;
-    //     }
-    // //     else 
-    // }
+// function validateForm(){
+//     let input = document.myForm.myBandBar.value;
+//     if (input == null || input == ""){
+//         alert ("Please enter an artist or band name");
+//         alert false;
+//     }
+// //     else
+// }
